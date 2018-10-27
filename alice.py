11e425124bd4fb897cbcb00d124b4c6da83cd2c6
Albert <albert.gisleberg@gmail.com>
2018-10-27 18:59:26 +0200

    1.1

--- alice.py
+++ alice.py
@@ -3,6 +3,22 @@ import _thread
 from Crypto import Random
 from Crypto.Cipher import AES
 
+def recive_message():
+	while 1:
+	    encrypted_msg = conn.recv(1024)
+	    print("zaszyfrowana wiadomość " + str(encrypted_msg))
+	    msg = (C.decrypt(encrypted_msg)).decode('utf-8')
+	    if msg == '':
+	        break
+	    print("po odszyfrowaniu: " + msg.strip('0') + '\n')
+
+def send_message():
+    while 1:
+        message = input()
+        encrypted_msg = C.encrypt(bytes(message, 'utf-8').zfill(((len(message) // 16)+ 1) * 16))
+        print("wysyłam: " + str(encrypted_msg))
+        conn.send(encrypted_msg)
+	
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = '127.0.0.1'
 s.bind((host, 5000))
@@ -11,13 +27,10 @@ conn, addr = s.accept()
 key = b'1234567890123456'
 C = AES.new(key, AES.MODE_ECB)
 
-while 1:
-    encrypted_msg = conn.recv(1024)
-    print("zaszyfrowana wiadomość " + str(encrypted_msg))
-    msg = (C.decrypt(encrypted_msg)).decode('utf-8')
-    if msg == '':
-        break
-    print("po odszyfrowaniu: " + msg.strip('0') + '\n')
+_thread.start_new_thread(recive_message, ())
+_thread.start_new_thread(send_message, ())
 
+while 1:
+	pass
 conn.close()
 
--- bob.py
+++ bob.py
@@ -1,6 +1,23 @@
 import socket
+import _thread
 from Crypto import Random
 from Crypto.Cipher import AES
+
+def recive_message():
+	while 1:
+	    encrypted_msg = s.recv(1024)
+	    print("zaszyfrowana wiadomość " + str(encrypted_msg))
+	    msg = (C.decrypt(encrypted_msg)).decode('utf-8')
+	    if msg == '':
+	        break
+	    print("po odszyfrowaniu: " + msg.strip('0') + '\n')
+
+def send_message():
+    while 1:
+        message = input()
+        encrypted_msg = C.encrypt(bytes(message, 'utf-8').zfill(((len(message) // 16)+ 1) * 16))
+        print("wysyłam: " + str(encrypted_msg))
+        s.send(encrypted_msg)
 
 s = socket.socket()
 host = '127.0.0.1'
@@ -10,8 +27,9 @@ C = AES.new(key, AES.MODE_ECB)
 
 s.connect((host, port))
 
+_thread.start_new_thread(recive_message, ())
+_thread.start_new_thread(send_message, ())
+
 while 1:
-    msg = input("wpisz wiadomość ->")
-    encrypted_msg = C.encrypt(bytes(msg, 'utf-8').zfill(((len(msg) // 16)+ 1) * 16))
-    print("wysyłam: " + str(encrypted_msg))
-    s.send(encrypted_msg)
+    pass
+
